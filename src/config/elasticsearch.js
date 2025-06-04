342105d47364b71982b585ab25081056fd38123e
VietThang1907 <hovietthang1907@gmail.com>
2025-06-04 09:02:56 +0700

    Update elasticsearch.js

--- src/config/elasticsearch.js
+++ src/config/elasticsearch.js
@@ -2,4 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     node: process.env.ELASTICSEARCH_NODE,
+    auth: {
+        apiKey: process.env.ELASTICSEARCH_API_KEY
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
 };
